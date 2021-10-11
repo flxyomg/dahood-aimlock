50ce1a298c0c2e39824e74d51c567e600e147cbd
flxyomg <92309509+flxyomg@users.noreply.github.com>
2021-10-11 14:53:09 +0300

    Update xppp.lua

--- xppp.lua
+++ xppp.lua
@@ -1,5 +1,5 @@
 -- // Dependencies
-local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Aiming/Module.lua"))()
+local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/flxyomg/dahood-aimlock/main/aimlock.lua"))()
 Aiming.TeamCheck(false)
 
 -- // Services
